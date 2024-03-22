58ca6182bb833b90832ba36bac0be0017b7736f7
moiezsyed <moiezhsyed@gmail.com>
2024-03-22 16:53:37 +0300

    Add imports and global vars

---
+++ TradingAlgo.py
@@ -0,0 +1,10 @@
+from lumibot.brokers import Alpaca  # broker
+from lumibot.backtesting import YahooDataBacktesting # backtesting framework
+from lumibot.strategies.strategy import Strategy # automated trading
+from lumibot.traders import Trader # deployement
+from datetime import datetime as dt
+
+API_KEY = "PKPRVPP06CANE9O63SN9"
+API_SECRET = "6FfKbsWFlbFHd4JEbnMaB337RTSHoDWIgRYZLPj7"
+API_ENDPOINT = "https://paper-api.alpaca.markets/v2"
+
