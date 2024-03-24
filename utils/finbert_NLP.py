74d14081be1c2a584d287984a00687c0fedcdfd9
moiezsyed <moiezhsyed@gmail.com>
2024-03-24 16:20:42 +0300

    Load the tokenizer; required for converting text inputs into tokens

--- utils/finbert_NLP.py
+++ utils/finbert_NLP.py
@@ -1,3 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+# Load the pre-trained model and Tokenizer
+tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
